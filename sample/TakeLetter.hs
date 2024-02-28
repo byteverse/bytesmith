139da01482d9d3526c0b70a196f6905b74867129
Brian McKeon <bmckeon@layer3com.com>
2024-02-28 09:04:48 -0500

    Prepare 0.3.11.1 release. Minor code cleanup. Updated changelog.

--- sample/TakeLetter.hs
+++ sample/TakeLetter.hs
@@ -1,6 +1,7 @@
-{-# language MagicHash #-}
+{-# LANGUAGE MagicHash #-}
 
 -- Build with:
+--   cabal build --write-ghc-environment-files=always
 --   ghc -fforce-recomp -O2 -ddump-simpl -dsuppress-all -ddump-to-file sample/TakeLetter.hs
 -- to examine GHC optimizations.
 module TakeLetter
@@ -8,10 +9,10 @@ module TakeLetter
   ) where
 
 import Data.Bytes.Parser (Parser)
-import Data.Text.Short (ShortText)
 import Data.Bytes.Parser.Ascii (takeShortWhile)
+import Data.Text.Short (ShortText)
 import GHC.Exts
 
 takeLetter :: Parser e s ShortText
-{-# noinline takeLetter #-}
+{-# NOINLINE takeLetter #-}
 takeLetter = takeShortWhile (== 'A')
